10cca7ad82f6750bfd487fd0fce566ab9e5eef13
cooollawf <115468608+windows11pe@users.noreply.github.com>
2024-05-01 21:58:27 +0800

    提交

---
+++ flash toolbox.bat
@@ -0,0 +1,122 @@
+@echo off
+chcp 936
+:: ------------------------------------------------------------------------------------------------------------------------------------------------------------
+::           OOO        OOOOOOOO      OOO        OOO          
+::           OOO        OOOOOOOO    OOOOOO     OOOOOO         
+::           OOO        OOOOOOOO   OOOOOOOO   OOOOOOOO        
+::           OOO        OOO        OOO   OOO  OOO   OO        
+::           OOO        OOO        OO    OOO  OOO   OO        
+::           OOO        OOO        OO    OOO  OOOO            
+::           OOO        OOOOOOOO   OO    OOO   OOOOO          toolbox V1 powerby cooollawf and 贡献者
+::           OOO        OOOOOOOO   OO    OOO    OOOOO                         
+::           OOO        OOO        OO    OOO      OOOO        
+::           OOO        OOO        OO    OOO        OOO       
+::           OOO        OOO        OO    OOO  OO    OOO       
+::           OOO        OOO        OOO   OOO  OOO   OOO       
+::           OOOOOOOOO  OOOOOOOOO  OOOOOOOO   OOOOOOOO        
+::           OOOOOOOOO  OOOOOOOOO   OOOOOO     OOOOOOO        
+::           OOOOOOOOO  OOOOOOOOO     OOO       OOOO          
+::______________________________________________________________________________________________________________________________________________________________
+:: part 01(主菜单)
+
+:menu
+cls
+echo           OOO        OOOOOOOO      OOO        OOO          
+echo           OOO        OOOOOOOO    OOOOOO     OOOOOO         
+echo           OOO        OOOOOOOO   OOOOOOOO   OOOOOOOO        
+echo           OOO        OOO        OOO   OOO  OOO   OO        
+echo           OOO        OOO        OO    OOO  OOO   OO        
+echo           OOO        OOO        OO    OOO  OOOO            
+echo           OOO        OOOOOOOO   OO    OOO   OOOOO          
+echo           OOO        OOOOOOOO   OO    OOO    OOOOO         
+echo           OOO        OOO        OO    OOO      OOOO        
+echo           OOO        OOO        OO    OOO        OOO       
+echo           OOO        OOO        OO    OOO  OO    OOO       
+echo           OOO        OOO        OOO   OOO  OOO   OOO       
+echo           OOOOOOOOO  OOOOOOOOO  OOOOOOOO   OOOOOOOO        
+echo           OOOOOOOOO  OOOOOOOOO   OOOOOO     OOOOOOO        
+echo           OOOOOOOOO  OOOOOOOOO     OOO       OOOO          
+echo “与你的相遇，就是奇迹，欢迎来到LEOS Toolbox工具箱V1”
+echo.
+echo 请选择一个选项：
+echo 1. 刷入/system目录下的system.img
+echo 2. 刷入boot
+echo 3. 自动刷入LEOS
+echo 4. 重启手机
+echo 5. 自带ADB CMD
+echo 6. 自动解锁BOOTLOADER Lock并刷入TWRP
+echo 7. 格式化userdata
+echo 8. 神奇妙妙小屋（没有开发）
+echo 9.导入system/boot资源
+echo 10.安装驱动
+echo.
+set /p choice=请输入您的选择（1-9）：
+if "%choice%"=="1" goto option1
+if "%choice%"=="2" goto option2
+if "%choice%"=="3" goto option3
+if "%choice%"=="4" goto option4
+if "%choice%"=="5" goto option5
+if "%choice%"=="6" goto option6
+if "%choice%"=="7" goto option7
+if "%choice%"=="8" goto option8
+if "%choice%"=="9" goto option9
+if "%choice%"=="10" goto option10
+echo 无效的选择，请重新输入。
+pause
+goto menu
+
+:option1
+fastboot flash system/system.img
+pause
+goto menu
+
+:option2
+fastboot flash boot/boot.img
+pause
+goto menu
+
+:option3
+echo 额。。。看起来还没有实现
+pause
+goto menu
+
+:option4
+fastboot reboot
+pause
+goto menu
+
+:option5
+start ADB.BAT
+pause
+goto menu
+
+:option6
+start unlock
+pause
+goto menu
+
+:option7
+fastboot erase userdata
+pause
+goto menu
+
+:option8
+echo echo 额。。。看起来还没有实现
+pause
+goto menu
+
+:option9
+echo 复制对应的文件（system.img/boot.img）到名字相对应的目录里面就可以了(system/boot目录）
+pause
+goto menu
+
+:option10
+echo 请等待安装驱动，安装完成后按下任意键以回到主页面
+start drv.exe
+pause
+goto menu
+
+pause
+:: -------------------------------------------------------------------------------------------------------------
+:: 结束这一行
+:: -------------------------------------------------------------------------------------------------------------
---
+++ unlock.bat
@@ -0,0 +1,5 @@
+fastboot oem unlock
+fastboot flash recovery TWRP/rec.img
+echo 完毕！请按下手机音量上和电源键重启到recvoery
+pause
+exit
